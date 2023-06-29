bfd88bc73df7ce2977fee88e0201ac076429380b
Du Shuchao <1104795501@qq.com>
2023-06-29 21:13:31 +0800

    Add files via upload

---
+++ envir/envir/源1.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//#include<GL/freeglut.h>
+//using namespace std;
+//
+//void renderWindow() {
+//    glClear(GL_COLOR_BUFFER_BIT);
+//    glColor3f(1, 0, 0);
+//    glBegin(GL_TRIANGLES);
+//    glVertex2f(-.5f, -.5f);
+//    glVertex2f(.5f, -.5f);
+//    glVertex2f(.0f, .5f);
+//    glEnd();
+//    glFlush();
+//}
+//
+//int main(int argc, char** argv)
+//{
+//    cout << argc << "\n" << *argv << endl;
+//    glutInit(&argc, argv);
+//    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+//    glutInitWindowPosition(1, 1);
+//    glutInitWindowSize(240, 240);
+//    glutCreateWindow("GL_Front");
+//    glutDisplayFunc(&renderWindow);
+//    glutMainLoop();
+//
+//    return 0;
+//}
